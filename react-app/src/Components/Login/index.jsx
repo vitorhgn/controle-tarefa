45119768e3cdbd23819930a6934f95662e1ac2a1
VITOR HUGO GOUVEIA NUNES <vitorhgn100@gmail.com>
2023-03-03 22:25:06 -0400

    instalado axios e formik para pagina de login

--- react-app/src/Components/Login/index.jsx
+++ react-app/src/Components/Login/index.jsx
@@ -1,41 +1,50 @@
 import React from "react";
 import "./style.css";
+import Axios from 'axios'
+import { Formik, Form, Field } from 'formik'
 
-function redireciona(){
-  window.location.href="/users";
-}
 
 export default function Login() {
   
+  // function redireciona(){
+  //   window.location.href="/users";
+  // }
+const handleClickLogin = (values)=>{
+   Axios.post("http://localhost:3009/auth/login", {
+    codigo: values.codigo,
+   senha: values.senha
+   }).then((response)=>{
+     console.log(response)
+  })
+}
+
   return (
     <section className="section-login">
-    <div className="container">
-      <form id="form-login" method="post">
+    <Formik className="container" initialValues={{}} onSubmit={handleClickLogin}>
+      <Form id="form-login">
         <div className="form-group">
-          <label htmlFor="inputLogin">Usuário</label>
-          <input
-            id="inputLogin"
-            required
-            type="text"
-            name="text"
+          <label htmlFor="codigo">Usuário</label>
+          <Field
+            id="codigo"
+            name="codigo"
+            type='text'
             placeholder="Digite seu usuário"
           />
         </div>
         <div className="form-group">
-          <label htmlFor="">Senha</label>
-          <input
-            id="inputPassword"
-            required
+          <label htmlFor="senha">Senha</label>
+          <Field
+            id="senha"
+            name="senha"
             type="password"
-            name="password"
             placeholder="Digite sua senha"
           />
         </div>
         <div className="button-group input-btn">
-          <input type="submit" value="Entrar" onClick={() => redireciona()}/>
+          <input type="submit" value="Entrar"/>
         </div>
-      </form>
-    </div>
+      </Form>
+    </Formik>
   </section>
   );
 }
